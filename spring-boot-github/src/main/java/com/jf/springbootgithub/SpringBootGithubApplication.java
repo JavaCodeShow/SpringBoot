7b94987953453acb129b1b12c09f868f8148db98
jiangfeng <1546844168@qq.com>
2018-10-08 13:05:27 +0800

    加了一个gitHub的组件

---
+++ spring-boot-github/src/main/java/com/jf/springbootgithub/SpringBootGithubApplication.java
@@ -0,0 +1,12 @@
+package com.jf.springbootgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGithubApplication.class, args);
+    }
+}
