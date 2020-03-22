0492f6f3b17fab285ff4c4e67570df5fb0c92da1
江峰 <1546844168@qq.com>
2020-03-22 09:02:56 +0800

    redis相关

--- springboot_redis_study/src/test/java/com/jf/redisstudy/RedisSpringBootTest.java
+++ springboot_redis_study/src/test/java/com/jf/redisstudy/RedisSpringBootTest.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 江峰
@@ -58,5 +61,39 @@ public class RedisSpringBootTest {
         System.out.println("查询redis耗时" + (end - start) + " ms");
         System.out.println("redis中key的个数：" + keys.size());
         redisTemplate.discard();
+    }
+
+    @Test
+    public void distributedLockTest() throws InterruptedException {
+        // ThreadPoolExecutor tpe = new ThreadPoolExecutor(5, 10, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(5));
+        int nThreads = 5;
+        ExecutorService es = Executors.newFixedThreadPool(nThreads);
+        for (int i = 0; i < 5; i++) {
+            es.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + " ：准备去获取锁 ");
+                    Boolean flag = redisTemplate.opsForValue().setIfAbsent("lock", "jf", 3, TimeUnit.SECONDS);
+                    System.out.println("hello");
+                    System.out.println("flag = " + flag);
+                    if (flag) {
+                        System.out.println(Thread.currentThread().getName() + " ：获取锁了，开始执行 ");
+                        try {
+                            TimeUnit.SECONDS.sleep(2);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } finally {
+                            redisTemplate.delete("lock");
+                            System.out.println(Thread.currentThread().getName() + " ：释放锁 ");
+                        }
+                        System.out.println(Thread.currentThread().getName() + " ：执行结束 ");
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " ：没有获取到锁 ");
+                    }
+                }
+            });
+        }
+        // es.shutdown();
+
     }
 }
---
+++ springboot_redis_study/src/test/java/com/jf/redisstudy/distributedLock/RedisDistributedLock.java
@@ -0,0 +1,11 @@
+package com.jf.redisstudy.distributedLock;
+
+/**
+ * @author 江峰
+ * @create 2020-03-21   20:18
+ */
+public class RedisDistributedLock {
+    public static void main(String[] args) {
+
+    }
+}
