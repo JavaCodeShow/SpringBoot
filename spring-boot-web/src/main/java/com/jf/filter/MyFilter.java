d4ffb73882e6797c388bc344099c81ff9e50f6ac
江峰 <1546844168@qq.com>
2020-03-24 21:56:13 +0800

    initial commit
    Signed-off-by: 江峰 <1546844168@qq.com>

--- spring-boot-web/src/main/java/com/jf/filter/MyFilter.java
+++ spring-boot-web/src/main/java/com/jf/filter/MyFilter.java
@@ -1,6 +1,10 @@
 package com.jf.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -8,21 +12,30 @@ import java.io.IOException;
  * @create 2018-09-30   23:20
  */
 public class MyFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(MyFilter.class);
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("filter 初始化了");
+    public void init(FilterConfig filterConfig) {
+        logger.info("初始化过滤器：", filterConfig.getFilterName());
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("filter 执行了");
-        // 放行
-        filterChain.doFilter(servletRequest,servletResponse);
+        //对请求进行预处理
+        logger.info("过滤器开始对请求进行预处理：");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String requestUri = request.getRequestURI();
+        System.out.println("请求的接口为：" + requestUri);
+        long startTime = System.currentTimeMillis();
+        //通过 doFilter 方法实现过滤功能
+        filterChain.doFilter(servletRequest, servletResponse);
+        // 上面的 doFilter 方法执行结束后用户的请求已经返回
+        long endTime = System.currentTimeMillis();
+        System.out.println("该用户的请求已经处理完毕，请求花费的时间为：" + (endTime - startTime));
     }
 
     @Override
     public void destroy() {
-        System.out.println("filter 销毁了");
+        logger.info("销毁过滤器");
     }
 }
