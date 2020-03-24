8e504cddbab19267287be8a828c295abea20244c
江峰 <1546844168@qq.com>
2020-03-24 21:25:30 +0800

    initial comit

--- spring-boot-web/src/main/java/com/jf/controller/MyExceptionHandler.java
+++ spring-boot-web/src/main/java/com/jf/controller/MyExceptionHandler.java
@@ -1,30 +1,31 @@
 package com.jf.controller;
 
 import com.jf.exception.UserNotExitException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author 潇潇暮雨
  * @create 2018-09-30   20:56
  */
+
+// 加上@ControllerAdvice注解这个类就成为了全局异常处理类
 @ControllerAdvice
+@ResponseBody
 public class MyExceptionHandler {
 
-    // 要加ResponseBody，因为返会的是一个json字符串
-//    @ResponseBody
+    // 拦截所有(UserNotExitException)异常, 一般情况下一个方法特定处理一种异常
     @ExceptionHandler(UserNotExitException.class)
-    public String handler(Exception e, HttpServletRequest request){
-        Map map = new HashMap<String,Object>();
-        map.put("code","有一行代码错了");
-        map.put("message","别睡了，起床改bug了！");
-        request.setAttribute("javax.servlet.error.status_code",404);
-        return "forward:/error";
+    public ResponseEntity<String> handler(Exception e) {
+        return ResponseEntity.status(404).body("后端出现了UserNotExitException");
     }
+/*
+    // 拦截所有异常, 这里只是为了演示，一般情况下一个方法特定处理一种异常
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handler1(Exception e) {
+        return ResponseEntity.status(404).body("后端出现了异常");
+    }*/
 
 }
