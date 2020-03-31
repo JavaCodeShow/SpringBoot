39801e0f2994e231f48d0c8602278b0a3a16bc28
江峰 <1546844168@qq.com>
2020-03-31 15:59:12 +0800

    使用webSocket实现点对点和全部发送消息

--- springboot_websocket/src/main/java/com/jf/websocket/controller/HelloController.java
+++ springboot_websocket/src/main/java/com/jf/websocket/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.jf.websocket.controller;
 
+import com.jf.websocket.ws.WebSocketServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * @author 江峰
@@ -10,8 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    // @Autowired
+    // private WebSocketServer webSocketServer;
+
     @RequestMapping("/hello")
     public String hello() {
+        try {
+            String userId = "11";
+            WebSocketServer.sendInfo("发送消息给" + userId, userId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "hello";
     }
 }
