ecffb1596b994e6e1f94569fa1cb7a98ccc37cb2
江峰 <1546844168@qq.com>
2020-03-31 22:41:40 +0800

    SpringBoo操作kafka
    Signed-off-by: 江峰 <1546844168@qq.com>

---
+++ spring-boot-kafka-study/src/main/java/com/jf/kafka/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.jf.kafka.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 潇潇暮雨
+ * @create 2018-10-05   10:36
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello kafka";
+    }
+
+    @RequestMapping("/success")
+    public String success() {
+        return "success";
+    }
+}
