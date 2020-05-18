080df3a720401ae8c73ce95661d7bcb1f042eceb
江峰 <1546844168@qq.com>
2020-05-18 11:28:25 +0800

    rocketmq环境搭建

---
+++ spring-boot-rockermq-study/src/test/java/com/jf/rockermqstudy/RockermqStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jf.rockermqstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockermqStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
