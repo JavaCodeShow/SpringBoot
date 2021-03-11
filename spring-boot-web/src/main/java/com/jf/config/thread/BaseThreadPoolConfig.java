1cf739df1d1a78b607f8003b9f0da2ea5991f276
江峰 <1546844168@qq.com>
2021-03-12 00:08:14 +0800

    线程池
    Signed-off-by: 江峰 <1546844168@qq.com>

---
+++ spring-boot-web/src/main/java/com/jf/config/thread/BaseThreadPoolConfig.java
@@ -0,0 +1,50 @@
+package com.jf.config.thread;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author 江峰
+ * @email feng.jiang@marketin.cn
+ * @create 2020-12-14 10:57:56
+ * @since
+ */
+@EnableAsync
+@Configuration
+@EnableConfigurationProperties({TaskThreadPoolProperties.class}) // 开启配置属性支持
+public class BaseThreadPoolConfig {
+
+    @Autowired
+    private TaskThreadPoolProperties threadPoolProperties;
+
+    private static final String THREAD_NAME = "baseAsyncExecutor";
+
+    @Bean(value = THREAD_NAME)
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //核心线程池大小
+        executor.setCorePoolSize(threadPoolProperties.getCorePoolSize());
+        //最大线程数
+        executor.setMaxPoolSize(threadPoolProperties.getMaxPoolSize());
+        //队列容量
+        executor.setQueueCapacity(threadPoolProperties.getQueueCapacity());
+        //活跃时间
+        executor.setKeepAliveSeconds(threadPoolProperties.getKeepAliveSeconds());
+        //线程名字前缀
+        executor.setThreadNamePrefix("TaskExecutePool-");
+
+        // setRejectedExecutionHandler：当pool已经达到max size的时候，如何处理新任务
+        // CallerRunsPolicy：不在新线程中执行任务，而是由调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 等待所有任务结束后再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+}
--- spring-boot-web/src/test/java/com/jf/SpringBootWebApplicationTests.java
+++ spring-boot-web/src/test/java/com/jf/SpringBootWebApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,6 +15,10 @@ public class SpringBootWebApplicationTests {
 
     @Autowired
     private OrderService orderService;
+
+    // @Autowired()
+    // @Qualifier("baseAsyncExecutor")
+    // private ThreadPoolTaskExecutor baseAsyncExecutor;
 
     @Test
     public void testSpringEvent() {
@@ -25,6 +30,19 @@ public class SpringBootWebApplicationTests {
     public void testPersonFactory() {
         PersonFactory.match(1).say();
         PersonFactory.match(2).say();
+    }
+
+    @Test
+    public void testThreadPool() {
+        // baseAsyncExecutor.execute(() -> {
+        //     System.out.println("hello");
+        // });
+        hello();
+    }
+
+    @Async("baseAsyncExecutor")
+    void hello() {
+        System.out.println("hello");
     }
 
 }
