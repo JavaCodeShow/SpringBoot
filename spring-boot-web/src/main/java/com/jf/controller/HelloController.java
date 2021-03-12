dda89e37aa60f7451204793fa22d23b41c3b2593
江峰 <feng.jiang@marketin.cn>
2021-03-12 18:30:15 +0800

    日志logback-config.xml配置

--- spring-boot-web/src/main/java/com/jf/controller/HelloController.java
+++ spring-boot-web/src/main/java/com/jf/controller/HelloController.java
@@ -7,6 +7,7 @@ import com.jf.entity.Employee;
 import com.jf.exception.UserNotExitException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
