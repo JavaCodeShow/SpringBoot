1c1b1b205d38135772e42a59713b8e2c2cb8a2d1
江峰 <feng.jiang@marketin.cn>
2021-03-18 16:23:40 +0800

    线程池配置

--- spring-boot-web/src/main/java/com/jf/config/thread/BaseThreadPoolConfig.java
+++ spring-boot-web/src/main/java/com/jf/config/thread/BaseThreadPoolConfig.java
@@ -9,15 +9,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import com.alibaba.fastjson.JSONObject;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author 江峰
  * @email feng.jiang@marketin.cn
  * @create 2020-12-14 10:57:56
  * @since
  */
-@EnableAsync
+
+@EnableAsync // 开启异步任务支持，然后才能使用@Async注解（@Async 注解作用应该使用在方法上，表示这个方法是异步方法。）
 @Configuration
-@EnableConfigurationProperties({ TaskThreadPoolProperties.class }) // 开启配置属性支持
+@EnableConfigurationProperties({ TaskThreadPoolProperties.class })
+@Slf4j
 public class BaseThreadPoolConfig {
 
 	@Autowired
@@ -28,16 +34,24 @@ public class BaseThreadPoolConfig {
 	@Bean(value = THREAD_NAME)
 	public ThreadPoolTaskExecutor taskExecutor() {
 
+		log.info("threadPoolProperties = [{}]",
+				JSONObject.toJSONString(threadPoolProperties));
+
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
 		// 核心线程池大小
 		executor.setCorePoolSize(threadPoolProperties.getCorePoolSize());
+
 		// 最大线程数
 		executor.setMaxPoolSize(threadPoolProperties.getMaxPoolSize());
+
 		// 队列容量
 		executor.setQueueCapacity(threadPoolProperties.getQueueCapacity());
+
 		// 活跃时间
 		executor.setKeepAliveSeconds(
 				threadPoolProperties.getKeepAliveSeconds());
+
 		// 线程名字前缀
 		executor.setThreadNamePrefix("TaskExecutePool-");
 
@@ -45,9 +59,11 @@ public class BaseThreadPoolConfig {
 		// CallerRunsPolicy：不在新线程中执行任务，而是由调用者所在的线程来执行
 		executor.setRejectedExecutionHandler(
 				new ThreadPoolExecutor.CallerRunsPolicy());
+
 		// 等待所有任务结束后再关闭线程池
 		executor.setWaitForTasksToCompleteOnShutdown(true);
 		executor.initialize();
+
 		return executor;
 	}
 }
