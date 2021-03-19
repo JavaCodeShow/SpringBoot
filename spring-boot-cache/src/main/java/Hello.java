7c3a79aef1186cecc213141e1c8e2d5c0c62ac70
江峰 <feng.jiang@marketin.cn>
2021-03-19 16:28:41 +0800

    删除无用项目

--- spring-boot-cache/src/main/java/Hello.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author 江峰
- * @create 2020-01-20   17:11
- */
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
-    }
-}
--- spring-boot-config-autoconfig/src/main/java/com/jf/bean/Dog.java
+++
@@ -1,9 +0,0 @@
-package com.jf.bean;
-
-/**
- * @author 潇潇暮雨
- * @create 2018-09-29   9:42
- */
-public class Dog {
-
-}
