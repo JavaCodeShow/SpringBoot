3ab4c2a77639a904d00b713a52b0910d5232c297
江峰 <feng.jiang@marketin.cn>
2021-03-27 00:59:23 +0800

    注册到nacos

--- template-ms/src/main/java/com/jf/css/TemplateMsApplication.java
+++ template-ms/src/main/java/com/jf/css/TemplateMsApplication.java
@@ -2,6 +2,7 @@ package com.jf.css;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import com.jf.common.redis.config.JfCommonRedisConfig;
@@ -9,6 +10,7 @@ import com.jf.common.utils.config.JfCommonUtilsConfig;
 
 @SpringBootApplication
 @Import({ JfCommonUtilsConfig.class, JfCommonRedisConfig.class })
+@EnableDiscoveryClient
 public class TemplateMsApplication {
 
 	public static void main(String[] args) {
