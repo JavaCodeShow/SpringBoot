d64714de83f584490b2c7ca4017b6114767856a4
江峰 <feng.jiang@marketin.cn>
2021-04-02 15:45:42 +0800

    producer

---
+++ spring-boot-rockermq-study/jf-rocketmq-producer/src/main/java/com/jf/rocketmqstudy/MQProducerApplication.java
@@ -0,0 +1,13 @@
+package com.jf.rocketmqstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MQProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MQProducerApplication.class, args);
+	}
+
+}
