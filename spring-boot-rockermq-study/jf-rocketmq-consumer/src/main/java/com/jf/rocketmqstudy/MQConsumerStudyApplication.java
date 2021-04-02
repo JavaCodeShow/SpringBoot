37ace60b643e046c9709be6e4dafac35409f270f
江峰 <feng.jiang@marketin.cn>
2021-04-02 15:45:31 +0800

    consumer

---
+++ spring-boot-rockermq-study/jf-rocketmq-consumer/src/main/java/com/jf/rocketmqstudy/MQConsumerStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jf.rocketmqstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MQConsumerStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MQConsumerStudyApplication.class, args);
+	}
+
+}
---
+++ spring-boot-rockermq-study/jf-rocketmq-consumer/src/main/java/com/jf/rocketmqstudy/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.jf.rocketmqstudy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+	@RequestMapping("/")
+	public String hello() {
+		log.info("hello world");
+		return "Hello RockerMQ consumer ";
+	}
+
+}
