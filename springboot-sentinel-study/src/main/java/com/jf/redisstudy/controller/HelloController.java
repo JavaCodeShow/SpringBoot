d5f86e29393ecdb763ecc6c29eac8c3ab3d11394
江峰 <feng.jiang@marketin.cn>
2021-05-10 18:34:59 +0800

    sentinel 项目搭建

--- springboot-sentinel-study/src/main/java/com/jf/redisstudy/controller/HelloController.java
+++ springboot-sentinel-study/src/main/java/com/jf/redisstudy/controller/HelloController.java
@@ -1,8 +1,17 @@
 package com.jf.redisstudy.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.jf.common.utils.result.BaseResult;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,5 +23,49 @@ public class HelloController {
 	@GetMapping("/")
 	public BaseResult index() {
 		return BaseResult.success("hello sentinel");
+	}
+
+	@GetMapping("/hello")
+	public BaseResult hello() {
+		// 配置规则.
+		initFlowRules();
+
+		while (true) {
+			// 1.5.0 版本开始可以直接利用 try-with-resources 特性，自动 exit entry
+			try (Entry entry = SphU.entry("HelloWorld")) {
+				// 被保护的逻辑
+				System.out.println("hello world");
+			} catch (BlockException ex) {
+				// 处理被流控的逻辑
+				System.out.println("blocked!");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// 配置规则.
+		initFlowRules();
+
+		while (true) {
+			// 1.5.0 版本开始可以直接利用 try-with-resources 特性，自动 exit entry
+			try (Entry entry = SphU.entry("HelloWorld")) {
+				// 被保护的逻辑
+				System.out.println("hello world");
+			} catch (BlockException ex) {
+				// 处理被流控的逻辑
+				System.out.println("blocked!");
+			}
+		}
+	}
+
+	private static void initFlowRules() {
+		List<FlowRule> rules = new ArrayList<>();
+		FlowRule rule = new FlowRule();
+		rule.setResource("HelloWorld");
+		rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+		// Set limit QPS to 20.
+		rule.setCount(20);
+		rules.add(rule);
+		FlowRuleManager.loadRules(rules);
 	}
 }
