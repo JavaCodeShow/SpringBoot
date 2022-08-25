2be06973bd8df4fd4290eef4788ea44a1a6cdd1d
江峰 <jiangfeng@morefungroup.com>
2022-08-25 11:17:54 +0800

    fix

--- spring-boot-rockermq-study/rocketmq-base-demo/src/main/java/com/jf/rocketmqstudy/mq/delayed/ScheduledMessageProducer.java
+++ spring-boot-rockermq-study/rocketmq-base-demo/src/main/java/com/jf/rocketmqstudy/mq/delayed/ScheduledMessageProducer.java
@@ -1,6 +1,6 @@
 package com.jf.rocketmqstudy.mq.delayed;
 
-import com.jf.common.utils.utils.time.LocalDateTimeUtil;
+import com.jf.common.utils.time.LocalDateTimeUtil;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
