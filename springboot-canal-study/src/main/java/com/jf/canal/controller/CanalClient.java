777f0b5c5d8900ebf45baada540a113fc6b4471e
江峰 <jiangfeng@morefungroup.com>
2022-09-09 18:52:03 +0800

    fix

--- springboot-canal-study/src/main/java/com/jf/canal/controller/CanalClient.java
+++ springboot-canal-study/src/main/java/com/jf/canal/controller/CanalClient.java
@@ -1,11 +1,11 @@
 package com.jf.canal.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.otter.canal.client.CanalConnector;
 import com.alibaba.otter.canal.client.CanalConnectors;
 import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.alibaba.otter.canal.protocol.Message;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.annotation.Lazy;
+import com.google.protobuf.ByteString;
 import org.springframework.stereotype.Component;
 
 import java.net.InetSocketAddress;
@@ -15,103 +15,67 @@ import java.util.List;
  * @author 江峰
  */
 @Component
-@Lazy(value = false)
-public class CanalClient implements InitializingBean {
+public class CanalClient {
 
-    private final static int BATCH_SIZE = 1000;
+    public static void main(String[] args) throws Exception {
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        // 创建链接
-        CanalConnector connector = CanalConnectors.newSingleConnector(new InetSocketAddress("139.224.103.236", 11111), "example", "", "");
-        try {
-            //打开连接
-            connector.connect();
-            //订阅数据库表,全部表
-            connector.subscribe(".*\\..*");
-            //回滚到未进行ack的地方，下次fetch的时候，可以从最后一个没有ack的地方开始拿
-            connector.rollback();
-            while (true) {
-                // 获取指定数量的数据
-                Message message = connector.getWithoutAck(BATCH_SIZE);
-                //获取批量ID
-                long batchId = message.getId();
-                //获取批量的数量
-                int size = message.getEntries().size();
-                //如果没有数据
-                if (batchId == -1 || size == 0) {
-                    try {
-                        //线程休眠2秒
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        //1.获取 canal 连接对象
+        CanalConnector canalConnector =
+                CanalConnectors.newSingleConnector(new
+                        InetSocketAddress("139.224.103.236", 11111), "example", "canal", "canal");
+
+        System.out.println("canal启动并开始监听数据 ...... ");
+        while (true) {
+            canalConnector.connect();
+            //订阅表
+            canalConnector.subscribe("shop001.*");
+            //获取数据
+            Message message = canalConnector.get(100);
+            //解析message
+            List<CanalEntry.Entry> entries = message.getEntries();
+            if (entries.size() <= 0) {
+                System.out.println("未检测到数据");
+                Thread.sleep(1000);
+            }
+            for (CanalEntry.Entry entry : entries) {
+                //1、获取表名
+                String tableName = entry.getHeader().getTableName();
+                //2、获取类型
+                CanalEntry.EntryType entryType = entry.getEntryType();
+                //3、获取序列化后的数据
+                ByteString storeValue = entry.getStoreValue();
+
+                //判断是否rowdata类型数据
+                if (CanalEntry.EntryType.ROWDATA.equals(entryType)) {
+                    //对第三步中的数据进行解析
+                    CanalEntry.RowChange rowChange = CanalEntry.RowChange.parseFrom(storeValue);
+                    //获取当前事件的操作类型
+                    CanalEntry.EventType eventType = rowChange.getEventType();
+                    //获取数据集
+                    List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
+                    //便利数据
+                    for (CanalEntry.RowData rowData : rowDatasList) {
+                        //数据变更之前的内容
+                        JSONObject beforeData = new JSONObject();
+                        List<CanalEntry.Column> beforeColumnsList = rowData.getAfterColumnsList();
+                        for (CanalEntry.Column column : beforeColumnsList) {
+                            beforeData.put(column.getName(), column.getValue());
+                        }
+                        //数据变更之后的内容
+                        List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+                        JSONObject afterData = new JSONObject();
+                        for (CanalEntry.Column column : afterColumnsList) {
+                            afterData.put(column.getName(), column.getValue());
+                        }
+                        System.out.println("Table ：" + tableName +
+                                ",eventType :" + eventType +
+                                ",beforeData :" + beforeData +
+                                ",afterData : " + afterData);
                     }
                 } else {
-                    //如果有数据,处理数据
-                    printEntry(message.getEntries());
-                }
-                //进行 batch id 的确认。确认之后，小于等于此 batchId 的 Message 都会被确认。
-                connector.ack(batchId);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            connector.disconnect();
-        }
-    }
-
-    /**
-     * 打印canal server解析binlog获得的实体类信息
-     */
-    private static void printEntry(List<CanalEntry.Entry> entrys) {
-        for (CanalEntry.Entry entry : entrys) {
-            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN || entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
-                //开启/关闭事务的实体类型，跳过
-                continue;
-            }
-            //RowChange对象，包含了一行数据变化的所有特征
-            //比如isDdl 是否是ddl变更操作 sql 具体的ddl sql beforeColumns afterColumns 变更前后的数据字段等等
-            CanalEntry.RowChange rowChage;
-            try {
-                rowChage = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
-            } catch (Exception e) {
-                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(), e);
-            }
-            //获取操作类型：insert/update/delete类型
-            CanalEntry.EventType eventType = rowChage.getEventType();
-            //打印Header信息
-            System.out.println(String.format("================》; binlog[%s:%s] , name[%s,%s] , eventType : %s",
-                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
-                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(),
-                    eventType));
-            //判断是否是DDL语句
-            if (rowChage.getIsDdl()) {
-                System.out.println("================》;isDdl: true,sql:" + rowChage.getSql());
-            }
-            //获取RowChange对象里的每一行数据，打印出来
-            for (CanalEntry.RowData rowData : rowChage.getRowDatasList()) {
-                //如果是删除语句
-                if (eventType == CanalEntry.EventType.DELETE) {
-                    printColumn(rowData.getBeforeColumnsList());
-                    //如果是新增语句
-                } else if (eventType == CanalEntry.EventType.INSERT) {
-                    printColumn(rowData.getAfterColumnsList());
-                    //如果是更新的语句
-                } else {
-                    //变更前的数据
-                    System.out.println("------->; before");
-                    printColumn(rowData.getBeforeColumnsList());
-                    //变更后的数据
-                    System.out.println("------->; after");
-                    printColumn(rowData.getAfterColumnsList());
+                    System.out.println("当前操作类型为：" + entryType);
                 }
             }
-        }
-    }
-
-    private static void printColumn(List<CanalEntry.Column> columns) {
-        for (CanalEntry.Column column : columns) {
-            System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
         }
     }
 }
