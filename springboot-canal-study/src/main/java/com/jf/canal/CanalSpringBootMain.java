4923bd5eedb793e94e68484956f1c6f170058475
江峰 <jiangfeng@morefungroup.com>
2022-09-09 18:37:54 +0800

    fix

---
+++ springboot-canal-study/src/main/java/com/jf/canal/CanalSpringBootMain.java
@@ -0,0 +1,17 @@
+package com.jf.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author 江峰
+ * @create 2020-03-21   10:56
+ */
+@ServletComponentScan("com.jf")
+@SpringBootApplication
+public class CanalSpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(CanalSpringBootMain.class, args);
+    }
+}
