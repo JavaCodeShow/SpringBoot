40d77b7da814823e53ba0371cbf320d9195124d2
江峰 <jiangfeng@morefungroup.com>
2024-04-15 15:55:27 +0800

    fix

--- spring-boot-mybatis-study/src/main/java/com/jf/mybatis/domain/param/account/AccountUpdateParam.java
+++ spring-boot-mybatis-study/src/main/java/com/jf/mybatis/domain/param/account/AccountUpdateParam.java
@@ -1,16 +1,22 @@
 package com.jf.mybatis.domain.param.account;
 
 import com.jf.model.request.BaseParam;
+import com.jf.model.request.ParamChecker;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class AccountUpdateParam extends BaseParam {
+
     private String id;
     private Integer money;
     private String userId;
 
     @Override
     public void checkParam() {
-
+        ParamChecker.notBlank(id, "id 不能为空");
+        ParamChecker.notNull(money, "money 不能为空");
+        ParamChecker.notBlank(userId, "userId 不能为空");
     }
 }
