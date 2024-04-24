973c2226c43f4f5952c74c94573487836b1e42c4
江峰 <jiangfeng@morefungroup.com>
2024-04-24 16:28:08 +0800

    fix

--- spring-boot-disruptor-study/src/main/java/com/jf/disruptor/message/MessageQueueManager.java
+++ spring-boot-disruptor-study/src/main/java/com/jf/disruptor/message/MessageQueueManager.java
@@ -23,7 +23,7 @@ public class MessageQueueManager {
                 factory,
                 bufferSize,
                 Executors.defaultThreadFactory(),
-                ProducerType.MULTI,
+                ProducerType.SINGLE,
                 new BlockingWaitStrategy());
 
         disruptor.handleEventsWith(new HelloEventHandler());
